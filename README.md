a6e5b1c24293bb9edc115a023f320c0672f3fc6a
Cole Willsea <coleww@gmail.com>
2015-07-04 16:04:25 -0700

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+diacriticize
+----------------
+
+randomly add some stackable unicode to a string
+
+### api
+
+`diacriticize(text, level=0.5)`
+- text: string to transform
+- level: optional float between 0-1 to compare Math.random() to when deciding whether or not to add a mark. higher => more marks
+
+### cli
+
---
+++ index.js
@@ -0,0 +1,13 @@
+var marks = "̴̵̶̷̸̡̢̧̨̛̖̗̘̙̜̝̞̟̠̣̤̥̦̩̪̫̬̭̮̯̰̱̲̳̹̺̻̼͇͈͉͍͎͓͔͕͖͙͚̀́̂̃̄̅̆̇̈̉̊̋̌̍̎̏̐̑̒̓̔̽̾̿̀́͂̓̈́͆͊͋͌͐͑͒͗͛ͣͤͥͦͧͨͩͪͫͬͭͮͯ̕̚͘͜͟͢͝͞͠͡ͅ͏".split("")
+
+module.exports = function(text, lev){
+  var level = lev !== undefined ? lev : 0.5
+  console.log(level)
+  return text.split("").map(function(character){
+    var first = Math.random() < level ? marks[~~(Math.random() * marks.length)] : ''
+    var last = Math.random() < level ? marks[~~(Math.random() * marks.length)] : ''
+    return first + character + last
+  }).join("")
+}
+
+module.exports.marks = marks
