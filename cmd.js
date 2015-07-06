d241204b04622a7efb7c6467d500fdc035c62bad
Cole Willsea <coleww@gmail.com>
2015-07-06 08:07:31 -0700

    use level option for piped input

--- cmd.js
+++ cmd.js
@@ -2,28 +2,26 @@
 
 var diacriticize = require('./')
 var fs = require('fs')
+var argv = require('yargs')
+  .alias('h', 'help')
+  .help('help')
+  .usage('randomly add diacritic unicode to a string')
+  .example('diacriticize lol cool whatever')
+  .example('diacriticize -l 0.1 oh yeah, wow so subtle')
+  .example('diacriticize -l 1 -f resume.txt')
+  .example('echo "WOW SO PRETTY" | diacriticize')
+  .alias('l', 'level')
+  .alias('f', 'file')
+  .describe('l', 'floating point frequency at which to add marks. higher => more marks')
+  .describe('f', 'path to file containing text to diacriticize')
+  .argv
 
 if(process.stdin.isTTY) {
-  var argv = require('yargs')
-    .alias('h', 'help')
-    .help('help')
-    .usage('randomly add diacritic unicode to a string')
-    .example('diacriticize lol cool whatever')
-    .example('diacriticize -l 0.1 oh yeah, wow so subtle')
-    .example('diacriticize -l 1 -f resume.txt')
-    .example('echo "WOW SO PRETTY" | diacriticize')
-    .alias('l', 'level')
-    .alias('f', 'file')
-    .describe('l', 'floating point frequency at which to add marks. higher => more marks')
-    .describe('f', 'path to file containing text to diacriticize')
-    .argv
-  var dict
   var text = argv.f ? fs.readFileSync(argv.f).toString() : argv._.join(" ")
 
   process.stdout.write(diacriticize(text, argv.l))
   process.stdout.write("\n")
 } else {
-
   var data = ''
   process.stdin.resume()
   process.stdin.setEncoding('utf8')
@@ -32,7 +30,7 @@ if(process.stdin.isTTY) {
   })
 
   process.stdin.on('end', function() {
-    process.stdout.write(diacriticize(data))
+    process.stdout.write(diacriticize(data, argv.l))
     process.stdout.write("\n")
   })
 }
